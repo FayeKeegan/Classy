5c3ea9ac2b4ac8aad733388e22a8dc143b4f28cc
Faye Keegan <faye.keegan@gmail.com>
2015-08-13 23:58:26 -0700

    classrooms index parses out desks

--- app/assets/javascripts/models/classroom.js
+++ app/assets/javascripts/models/classroom.js
@@ -1,3 +1,18 @@
 SeatingApp.Models.Classroom = Backbone.Model.extend({
-	urlRoot: "api/classrooms"
+	urlRoot: "api/classrooms",
+
+  parse: function (response) {
+    if (response.desks){
+    	this.desks().set(response.desks)
+    	delete response.desks
+    }
+    return response;
+  },
+
+  desks: function () {
+    if (!this._desks) {
+      this._desks = new SeatingApp.Collections.Desks();
+    }
+    return this._desks;
+  }
 })
