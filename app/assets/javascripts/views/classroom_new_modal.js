d2286a4c6866a1a45a6aae3c7e1baab9662770b6
Faye Keegan <faye.keegan@gmail.com>
2015-08-16 11:45:58 -0700

    add classroom grid to new classroom modal

--- app/assets/javascripts/views/classroom_new_modal.js
+++ app/assets/javascripts/views/classroom_new_modal.js
@@ -9,6 +9,11 @@ SeatingApp.Views.ClassroomNewModal = Backbone.View.extend({
 
   initialize: function () {
     $(document).on('keyup', this.handleKey.bind(this));
+    this.setDefaultClassroomSize();
+  },
+
+  setDefaultClassroomSize: function(){
+    this.model.set({height: 8, width:10})
   },
 
   handleKey: function (event) {
@@ -22,8 +27,19 @@ SeatingApp.Views.ClassroomNewModal = Backbone.View.extend({
     this.remove();
   },
 
+  addGridToPage: function(){
+    for (var i = 0; i < this.model.get("height"); i++) {
+      var row = $("<tr>").addClass("classroom-row-modal")
+      var find = $("#classroom-grid")
+      $("#classroom-grid").append(row)
+      for (var j = 0; j < this.model.get("width"); j++) {
+        var cell = $("<td>").addClass("classroom-square-modal").attr("row-num", i).attr("col-num", j)
+        row.append(cell)
+      }
+    }
+  },
+
   createClassroom: function (event) {
-    
     event.preventDefault();
     var formData = $(event.currentTarget).serializeJSON();
     this.model.save(formData, {
@@ -37,6 +53,7 @@ SeatingApp.Views.ClassroomNewModal = Backbone.View.extend({
   render: function () {
     var content = this.template()
     this.$el.html(content);
+    this.addGridToPage();
     return this;
   },
 
